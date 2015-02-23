57e21dd193549c60aed794c87d62f1b84a58cd60
shrutipatel123 <shruti.patel72@gmail.com>
2015-02-23 05:41:57 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,8 +13,12 @@ list(set=set, get=get,
    setmatrix=setmatrix,
    getmatrix=getmatrix)
 }
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
 
-
+# This function assumes that the matrix is always invertible.
 
 
 cacheSolve <- function(x, ...) {
@@ -28,3 +32,24 @@ cacheSolve <- function(x, ...) {
     x$setmatrix(m)
     m
 }
+## Sample run:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
